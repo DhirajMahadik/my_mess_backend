ebe1789ed9c52ee1506029756a778054c621a87f
Dhiraj <dhiraj.mobicloud@gmail.com>
2023-03-01 10:49:51 +0530

    NA

--- DB/config.js
+++ DB/config.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 // const URL = process.env.DATABASE
 // configuration of backend with database
 const Connect = () => {
-
-    // return mongoose.connect('mongodb+srv://dhirajdemo9221:dhirajdemo%40123@cluster0.10xjk3l.mongodb.net/test')
-    return mongoose.connect(process.env.DATABASE)
+    // console.log(process.env.DATABASE)
+    return mongoose.connect(`mongodb+srv://${process.env.DATABASE}/test`)
+    // return mongoose.connect(process.env.DATABASE)
+   
 
 }
 
