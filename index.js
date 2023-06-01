c9f19ca96ce81f78c01564b45b63397c497f878d
Dhiraj Mahadik <dhirajmahadik9221@gmail.com>
2023-06-01 17:08:05 +0530

    changes in config

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ cloudinary.config({
     cloud_name: "djmp17jsh",
     api_key: "144788257939745",
     api_secret: "pU3s1YTh-zNkit2umw9S2YABNfk"
-  });
-  
+});
+
 
 // Folder creation for user images
 
@@ -43,7 +43,7 @@ const maxSize = 10 * 1024 * 1024
 const upload = multer({
     storage: multer.diskStorage({}),
     limits: { fileSize: maxSize },
-    fileFilter:  (req, file , cb)=>{
+    fileFilter: (req, file, cb) => {
         let ext = path.extname(file.originalname);
         // if( ext !== ".jpg" && !== ".jpeg" && !== ".png" ){
         //     cb( new Error("File type is not supported"), false);
@@ -52,22 +52,24 @@ const upload = multer({
         cb(null, true)
     }
 
-  });
+});
 
 // secret key for auth token
 const secretKey = "Dhiraj"
 
 //taking auth token from headers
-const verify_token =(req, res, next)=>{
+const verify_token = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
-    if(typeof bearerHeader === 'undefined'){
-        res.send({result :'invalid token'})
-    }else{
-        const bearer = bearerHeader.split(" ");
+    if (typeof bearerHeader === 'undefined') {
+        res.send({ result: 'invalid token' })
+    } else {
+        const bearer = bearerHeader.split(" ")
+
+            ;
         const token = bearer[1]
         req.token = token
         next();
-        
+
     }
 }
 
@@ -78,7 +80,7 @@ app.get('/', async (req, res) => {
 })
 
 // User registration route 
-app.post('/add-mess',  async (req, res) => {
+app.post('/add-mess', async (req, res) => {
     const email = req.body.email;
     let data0 = await Mess.findOne({ email: email });
     if (!data0) {
@@ -118,7 +120,7 @@ app.get('/mess/:id', async (req, res) => {
     let data = await Mess.findOne({ _id: req.params.id })
 
     if (data) {
-        res.send({messname: data.messname, type:data.type, open:data.mess_open, close:data.mess_close, location:data.location, phone:data.phone, address:data.address, image:data.image, email:data.email , photos:data.photos, lunch_time:data.lunch_time, dinner_time:data.dinner_time, lunch_price:data.lunch_price, dinner_price:data.dinner_price, lunch_menu:data.lunch_menu , dinner_menu:data.dinner_menu })
+        res.send({ messname: data.messname, type: data.type, open: data.mess_open, close: data.mess_close, location: data.location, phone: data.phone, address: data.address, image: data.image, email: data.email, photos: data.photos, lunch_time: data.lunch_time, dinner_time: data.dinner_time, lunch_price: data.lunch_price, dinner_price: data.dinner_price, lunch_menu: data.lunch_menu, dinner_menu: data.dinner_menu })
     }
     else {
         res.send("No data found")
@@ -140,25 +142,25 @@ app.get('/search/:key', async (req, res) => {
 // User login 
 app.post('/login', async (req, res) => {
     const email = req.body.email;
-    
+
 
     let data = await Mess.findOne({ email: email, })
     // console.log(data)
 
-   let pass = await bcrypt.compare( req.body.password, data.password);
-//    console.log(pass)
-//    res.send(data._id)
+    let pass = await bcrypt.compare(req.body.password, data.password);
+    //    console.log(pass)
+    //    res.send(data._id)
 
     if (pass) {
-       console.log(data._id)
+        console.log(data._id)
         JWT.sign({ email, _id: data._id }, secretKey, { expiresIn: '900s' }, (err, token) => {
-            if(err){
+            if (err) {
                 res.status(400).send(err)
-            }else{
+            } else {
                 res.status(200).json({ token })
             }
-            
-            
+
+
         })
 
     }
@@ -168,17 +170,17 @@ app.post('/login', async (req, res) => {
 })
 
 //profile route 
-app.get('/profile', verify_token, (req, res)=>{
-    JWT.verify(req.token, secretKey, (err, authData)=>{
+app.get('/profile', verify_token, (req, res) => {
+    JWT.verify(req.token, secretKey, (err, authData) => {
         console.log(authData)
-        if (err){
+        if (err) {
             res.send(err)
-        }else{
+        } else {
             Mess.findById(mongoose.Types.ObjectId(authData._id)).select('messname messtype mess_close mess_open location phone address image email photos lunch_time lunch_price dinner_time dinner_price lunch_menu dinner_menu').then((messData) => {
                 res.send(messData)
             })
         }
-       
+
         // if(err){
         //     console.log( err)
         // }else{
@@ -189,109 +191,109 @@ app.get('/profile', verify_token, (req, res)=>{
     })
 })
 
-app.put('/update-profile', async(req,res)=>{
+app.put('/update-profile', async (req, res) => {
     console.log(req.body)
-    let data = Mess.findOne({_id:req.body._id})
-    if(data){
+    let data = Mess.findOne({ _id: req.body._id })
+    if (data) {
         let data1 = await Mess.updateOne(
-           {_id: req.body._id},
+            { _id: req.body._id },
             {
-              $set: req.body
-        
+                $set: req.body
+
             }
-          )
-          res.send(data1);
-    }else{
-        res.send({err:"something wrong"})
+        )
+        res.send(data1);
+    } else {
+        res.send({ err: "something wrong" })
     }
 
-    
+
 })
 
-app.post('/add-image',upload.single('image') , (req,res)=>{
+app.post('/add-image', upload.single('image'), (req, res) => {
     console.log(req.body)
     console.log(req.file.path)
     // res.send(req.file.path)
     // const file = req.files.image
-    cloudinary.uploader.upload(req.file.path, async (err, result)=>{
+    cloudinary.uploader.upload(req.file.path, async (err, result) => {
         console.log(result.url)
-       let data = await Mess.updateOne(
-            {_id: req.body._id},
+        let data = await Mess.updateOne(
+            { _id: req.body._id },
             {
-              $set: {image : result.url}
-        
+                $set: { image: result.url }
+
             }
         )
         res.send(data)
-    }).then(()=>{
+    }).then(() => {
         let previous_url = req.body.previous
         let arr = previous_url.split("/")
-        let provisional_img_name = arr[arr.length - 1 ]
+        let provisional_img_name = arr[arr.length - 1]
         let img_name = provisional_img_name.split(".")
         let actual_img_name = img_name[0]
         console.log(actual_img_name)
-        cloudinary.uploader.destroy(actual_img_name, async (err, result)=>{
+        cloudinary.uploader.destroy(actual_img_name, async (err, result) => {
             console.log(result)
             console.log(err)
-          
+
         })
     })
-   
+
 })
 
-app.post('/add-collection-image', upload.single("image"),  (req,res)=>{
+app.post('/add-collection-image', upload.single("image"), (req, res) => {
     console.log(req.body)
     // const file = req.files.image
-    cloudinary.uploader.upload(req.file.path, async (err, result)=>{
+    cloudinary.uploader.upload(req.file.path, async (err, result) => {
         console.log(result.url)
-        let messData = await Mess.findOne({_id:req.body._id})
+        let messData = await Mess.findOne({ _id: req.body._id })
         let arr = messData.photos
         arr.push(result.url)
-       let data = await Mess.updateOne(
-            {_id: req.body._id},
+        let data = await Mess.updateOne(
+            { _id: req.body._id },
             {
-              $set: {photos :arr}
-        
+                $set: { photos: arr }
+
             }
         )
         res.send(data)
     })
-   
+
 })
 
-app.post('/remove-collection-image', async (req , res)=>{
+app.post('/remove-collection-image', async (req, res) => {
     console.log(req.body)
 
-    let messData = await Mess.findOne({_id:req.body._id})
-        let arr1 = messData.photos
-        let Index = arr1.findIndex((element)=> element === req.body.previous)
-            arr1.splice(Index , 1)
-        Mess.updateOne(
-            {_id: req.body._id},
-            {
-              $set: {photos :arr1}
-        
-            }
-        ).then(()=>{
+    let messData = await Mess.findOne({ _id: req.body._id })
+    let arr1 = messData.photos
+    let Index = arr1.findIndex((element) => element === req.body.previous)
+    arr1.splice(Index, 1)
+    Mess.updateOne(
+        { _id: req.body._id },
+        {
+            $set: { photos: arr1 }
 
-            let previous_url = req.body.previous
-            let arr = previous_url.split("/")
-            let provisional_img_name = arr[arr.length - 1 ]
-            let img_name = provisional_img_name.split(".")
-            let actual_img_name = img_name[0]
-            console.log(actual_img_name)
-            cloudinary.uploader.destroy(actual_img_name, async (err, result)=>{
-                console.log(result)
-                console.log(err)
-                res.send(result)
-              
-            })
+        }
+    ).then(() => {
+
+        let previous_url = req.body.previous
+        let arr = previous_url.split("/")
+        let provisional_img_name = arr[arr.length - 1]
+        let img_name = provisional_img_name.split(".")
+        let actual_img_name = img_name[0]
+        console.log(actual_img_name)
+        cloudinary.uploader.destroy(actual_img_name, async (err, result) => {
+            console.log(result)
+            console.log(err)
+            res.send(result)
+
         })
-        
+    })
 
-   
 
-   
+
+
+
 })
 
 
